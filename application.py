da954706419c9c2fef0e9732df4d44b8d2ee1c01
Shengtao Xu <shengtao.xu@oracle.com>
2019-02-18 07:01:14 -0600

    add additem page and category page

--- application.py
+++ application.py
@@ -5,7 +5,8 @@ from database_setup import Base, Category, Item
 
 app = Flask(__name__)
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('sqlite:///catalog.db',
+                       connect_args={'check_same_thread': False})
 Base.metadata.bind = engine
 
 DBSession = sessionmaker(bind=engine)
@@ -21,32 +22,43 @@ def latestItems():
 
 @app.route('/catalog/<string:category_name>/')
 def categoryItems(category_name):
-    return "page of the list of items of " + category_name
+    categories = session.query(Category).all()
+    items = session.query(Item).filter_by(category_name=category_name).all()
+    return render_template('categoryitem.html', categories=categories, category_name=category_name, items=items)
 
 
-@app.route('/catalog/<string:category_name>/<string:item_name>/')
-def itemInfo(category_name, item_name):
-    return "page of information of " + item_name + " in category " + category_name
+@app.route('/catalog/<string:category_name>/<int:item_id>/')
+def itemInfo(category_name, item_id):
+    return "page of information of " + item_id + " in category " + category_name
 
 
-@app.route('/catalog/<string:category_name>/<string:item_name>/json')
-def itemJSON(category_name, item_name):
-    return "json of " + item_name + " in category " + category_name
+@app.route('/catalog/<string:category_name>/<int:item_id>/json')
+def itemJSON(category_name, item_id):
+    return "json of " + item_id + " in category " + category_name
 
 
 @app.route('/item/add/', methods=['GET', 'POST'])
 def addItem():
-    return "page to add an item"
+    if request.method == 'POST':
+        categories = session.query(Category).all()
+        newItem = Item(
+            name=request.form['name'], description=request.form['description'], category_name=request.form['category'])
+        session.add(newItem)
+        session.commit()
+        return redirect(url_for('categoryItems', category_name=request.form['category']))
+    else:
+        categories = session.query(Category).all()
+        return render_template('additem.html', categories=categories)
 
 
-@app.route('/item/<string:item_name>/edit/', methods=['GET', 'POST'])
-def editItem(item_name):
-    return "edit item " + item_name
+@app.route('/item/<int:item_id>/edit/', methods=['GET', 'POST'])
+def editItem(item_id):
+    return "edit item " + item_id
 
 
-@app.route('/item/<string:item_name>/delete/', methods=['GET', 'POST'])
-def deleteItem(item_name):
-    return "delete item " + item_name
+@app.route('/item/<int:item_id>/delete/', methods=['GET', 'POST'])
+def deleteItem(item_id):
+    return "delete item " + item_id
 
 
 @app.route('/catalog.json/')
--- database_setup.py
+++ database_setup.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy import Column, ForeignKey, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
@@ -24,7 +24,8 @@ class Category(Base):
 class Item(Base):
     __tablename__ = 'item'
 
-    name = Column(String(50), nullable=False, primary_key=True)
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50), nullable=False)
     description = Column(String(500))
     category_name = Column(String, ForeignKey('category.name'))
     category = relationship(Category)
@@ -33,13 +34,14 @@ class Item(Base):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {
+            'id': self.id,
             'name': self.name,
             'description': self.description,
             'category_name': self.category_name,
         }
 
 
-engine = create_engine('sqlite:///catalog.db')
+engine = create_engine('sqlite:///catalog.db', connect_args={'check_same_thread': False})
 
 Base.metadata.create_all(engine)
 
